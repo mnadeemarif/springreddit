5ae06578c58f0a001526d52ac4cf4d9068bf368c
nadeem.arif <afzalbrothers1@gmail.com>
2022-06-16 16:52:32 +0500

    update cors configuration

--- src/main/java/com/nadeemarif/reddit/config/SecurityConfig.java
+++ src/main/java/com/nadeemarif/reddit/config/SecurityConfig.java
@@ -41,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors()
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
--- src/main/java/com/nadeemarif/reddit/config/WebConfig.java
+++ src/main/java/com/nadeemarif/reddit/config/WebConfig.java
@@ -28,5 +28,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
 }
